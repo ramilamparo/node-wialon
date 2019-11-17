76f1a82c81d1c8301ef8020de136d0c42d75febf
Ramil Amparo <ramilamparo@gmail.com>
2019-11-18 01:37:56 +0400

    Fix typings.

--- utils/wialon/RemoteAPI.ts
+++ utils/wialon/RemoteAPI.ts
@@ -33,7 +33,7 @@ export abstract class RemoteAPI {
 		params?: Params[T] | null,
 		sid?: string | null,
 		url: string = defaultHost
-	): Promise<Response[T] | WialonError> => {
+	): Promise<Response[T]> => {
 		const formData = new FormData();
 		if (params) {
 			formData.append("params", JSON.stringify(params));
