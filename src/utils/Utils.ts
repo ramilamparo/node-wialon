bf56b7356b4ba14ae6862adf620cc59709c7d208
ramilamparo <ramilamparo@gmail.com>
2021-03-17 15:54:06 +0400

    Update get address api.

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -4,6 +4,28 @@ import { RemoteAPI } from "../RemoteAPI";
 import { Core } from "../core";
 import { UnitsDataFormat } from "../format/Units";
 
+export interface UtilsGetAddressCoords {
+	lat: number;
+	lng: number;
+}
+
+export interface UtilsGetAddressParams {
+	/** array with coordinates */
+	coords: UtilsGetAddressCoords[];
+	/** minimal city radius, km (optional). Minimal city radius says that if no city has been found within maximum distance from unit (the previous option), then the address is bound to another city. How big this city should be to form the address, you indicate here – minimum radius of a city. This can be used to eliminate small cities from addresses. */
+	city_radius?: number;
+	/** maximal distance from unit, km (optional). Maximal distance from unit says that if unit is located near a road and there is a city (i.e. city, town, village) within the indicated distance, then in the address you will have the road's name and distance to the city. */
+	dist_from_unit?: number;
+	/**  metrics of maximal distance from unit (optional) */
+	txt_dist?: number;
+	/** flags of output format (see below) */
+	flags: number;
+	/** current user ID  */
+	uid: number;
+}
+
+export type UtilsGetAddressResponse = string[];
+
 export class Utils extends RemoteAPI {
 	public getUnits = <Response = UnitsDataFormat.GeneralProperties>({
 		flags = 1
@@ -24,18 +46,24 @@ export class Utils extends RemoteAPI {
 		});
 	};
 
-	public getAddress = async (
-		{ lat, lng }: { lat: number; lng: number },
-		flags = 54321
-	) => {
+	public getAddress = async (params: UtilsGetAddressParams) => {
 		const formData = new FormData();
 		const authDetails = await this.getAuthDetails();
 		formData.append("uid", String(authDetails.user.id));
 		formData.append("sid", this.sessionId);
-		formData.append("flags", String(flags));
-		formData.append("coords", JSON.stringify([{ lat, lng }]));
+		formData.append("flags", String(params.flags));
+		formData.append("coords", JSON.stringify(params.coords));
+		if (params.city_radius) {
+			formData.append("flags", String(params.city_radius));
+		}
+		if (params.dist_from_unit) {
+			formData.append("flags", String(params.dist_from_unit));
+		}
+		if (params.txt_dist) {
+			formData.append("flags", String(params.txt_dist));
+		}
 
-		const res = await axios.post(
+		const res = await axios.post<UtilsGetAddressResponse>(
 			`https://geocode-maps.wialon.com/${this.options.host.replace(
 				"https://",
 				""
@@ -45,6 +73,7 @@ export class Utils extends RemoteAPI {
 				timeout: 0
 			}
 		);
+
 		return res.data;
 	};
 }
