10af21a332ee15e3f536100304a4d4f51bb794df
ramilamparo <ramilamparo@gmail.com>
2021-03-17 09:15:49 +0400

    Fix core/batch params typings.

--- dist/core/batch.d.ts
+++ dist/core/batch.d.ts
@@ -2,5 +2,4 @@ import type { SVC } from "../types";
 export interface Params<T> {
     svc: SVC;
     params: T;
-    flag: number;
 }
--- src/core/batch.ts
+++ src/core/batch.ts
@@ -3,5 +3,4 @@ import type { SVC } from "../types";
 export interface Params<T> {
 	svc: SVC;
 	params: T;
-	flag: number;
 }
