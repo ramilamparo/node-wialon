35be09ea9236c425253b7ccf484ea8504839ae38
EngrPork <francisco.carlitos12@gmail.com>
2021-03-23 09:53:41 +0400

    build

--- dist/orders/Orders.d.ts
+++ dist/orders/Orders.d.ts
@@ -1,6 +1,24 @@
 import { RemoteAPI } from "../RemoteAPI";
 import type { Params as UseOrdersParams } from "./use_order";
 export declare class Orders extends RemoteAPI {
+    /**
+     * All of the required Fields
+     * @n Order Name
+     * p.@n Supplier Name
+     * p.@v Phone Number 1
+     * p.@v Email
+     * p.@v Volume
+     * p.@v Volume
+     * p.@w Weight
+     * p.@c Cost
+     * p.@d Description
+     * p.@t Vehicle Type
+     * p.@cid Custom Id
+     * @tf Allowed Timeframe - Start, UNIX-time
+     * @tt Allowed Timeframe - End, UNIX-time
+     * @y latitude
+     * @x longitude
+     */
     createOrder: (params: UseOrdersParams<"create">) => Promise<[number, import("./use_order").CreateResponse]>;
     updateOrder: (params: UseOrdersParams<"update">) => Promise<number[]>;
     deleteOrder: (params: UseOrdersParams<"delete">) => Promise<[number, [number, null]]>;
--- dist/orders/Orders.js
+++ dist/orders/Orders.js
@@ -5,6 +5,24 @@ const RemoteAPI_1 = require("../RemoteAPI");
 class Orders extends RemoteAPI_1.RemoteAPI {
     constructor() {
         super(...arguments);
+        /**
+         * All of the required Fields
+         * @n Order Name
+         * p.@n Supplier Name
+         * p.@v Phone Number 1
+         * p.@v Email
+         * p.@v Volume
+         * p.@v Volume
+         * p.@w Weight
+         * p.@c Cost
+         * p.@d Description
+         * p.@t Vehicle Type
+         * p.@cid Custom Id
+         * @tf Allowed Timeframe - Start, UNIX-time
+         * @tt Allowed Timeframe - End, UNIX-time
+         * @y latitude
+         * @x longitude
+         */
         this.createOrder = (params) => RemoteAPI_1.RemoteAPI.execute("order/update", params, this.sessionId, this.options.host);
         this.updateOrder = (params) => RemoteAPI_1.RemoteAPI.execute("order/update", params, this.sessionId, this.options.host);
         this.deleteOrder = (params) => RemoteAPI_1.RemoteAPI.execute("order/update", params, this.sessionId, this.options.host);
